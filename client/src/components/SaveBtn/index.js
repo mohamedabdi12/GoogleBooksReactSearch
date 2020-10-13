44cf73bb1f055aedd7f3c2312c4d534ce2c9de6e
Mohamed Abdi <lovegoldone@gmail>
2020-10-12 21:31:24 -0700

    made it

---
+++ client/src/components/SaveBtn/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+import { Modal, Button } from "react-bootstrap";
+
+
+function SaveBtn({ type = "default", className, children, onClick }) {
+
+    const [show, setShow] = useState(false);
+
+
+    const handleClose = () => setShow(false);
+
+    const handleShow = () => setShow(true);
+
+    const click = () => {
+        handleShow()
+        onClick()
+    };
+
+    return (
+        <>
+            <button onClick={click} className={["btn btn-lg", `btn-${type}`, className].join(" ")}>
+                {children}
+            </button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>You're book has been saved</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>You can view your saved books in the Saved tab</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+            </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        OK
+            </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+};
+
+export default SaveBtn;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const apiRoutes = require("./routes/index");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use('/api', apiRoutes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks")
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
